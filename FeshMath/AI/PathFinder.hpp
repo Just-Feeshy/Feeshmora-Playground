9e41d2b7ceda6531474634b32f26f8039c1b4531
Feeshy Fesh <58647349+Just-Feeshy@users.noreply.github.com>
2022-03-29 18:45:05 +0000

    add a distance method to the PathFinder header.

--- FeshMath/AI/PathFinder.hpp
+++ FeshMath/AI/PathFinder.hpp
@@ -5,11 +5,21 @@
 
 #include <cmath>
 
+/**
+* Basic math header.
+*/
 namespace PathFinder {
     static glm::vec3 getAngleThroughPath(const glm::vec3 &v1, const glm::vec3 &v2) {
         const float y1 = atan2(v2.z - v1.z, v2.x - v1.x);
 
         return glm::vec3(0, -glm::degrees(y1), 0);
+    }
+
+    /**
+    * 3D Pythagorean Theorem leak lmao.
+    */
+    static float getDistance(const glm::vec3 &v1, const glm::vec3 &v2) {
+        return sqrt(pow(v2.x - v1.x, 2) + pow(v2.y - v1.y, 2) + pow(v2.z - v1.z, 2));
     }
 }
 #endif
